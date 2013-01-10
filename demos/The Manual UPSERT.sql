958c0dee3df1916f37e342abf1e01f2b2ac83662
Mark S. Rasmussen <mark@improve.dk>
2013-01-11 00:15:38 +0100

    Updated The Manual UPSERT demo code

--- demos/The Manual UPSERT.sql
+++ demos/The Manual UPSERT.sql
@@ -17,3 +17,14 @@ INSERT INTO Pages (Name, Hits) VALUES ('Test', 1)
 UPDATE Pages SET Hits = Hits + 1 WHERE Name = 'Test'
 
 ROLLBACK
+
+MERGE
+	Pages AS Target
+USING
+	(SELECT 'Test', 1) AS Source (Name, Hits)
+ON
+	Target.Name = Source.Name
+WHEN MATCHED THEN
+	UPDATE SET Target.Hits = Target.Hits + Source.Hits
+WHEN NOT MATCHED BY TARGET THEN
+	INSERT (Name, Hits) VALUES (Source.Name, Source.Hits);
